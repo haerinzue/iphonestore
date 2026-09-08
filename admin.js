ecb91a2c716092f4f701eaa7fdcd2ad092c97ee8
haerinzue <enixela22@gmail.com>
2026-09-09 07:44:42 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,12 @@
 const SUPABASE_URL = 'https://fvxpfpqkdsznvvreicfc.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_dC4BDvHAExevhXghB6-8rQ_RLtR12zB';
-const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: true,
+    detectSessionInUrl: true
+  }
+});
 
 const $ = id => document.getElementById(id);
 const loginScreen = $('loginScreen');
